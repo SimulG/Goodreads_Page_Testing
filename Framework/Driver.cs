655327aa532bdec03b28081304e79626e53a0e07
simul <simulis.gediminas@gmail.com>
2022-07-06 13:57:13 +0300

    Add project files.

---
+++ Framework/Driver.cs
@@ -0,0 +1,37 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.IO;
+
+namespace Framework
+{
+    public class Driver
+    {
+        public static IWebDriver driver;
+        
+        public static void setDriver()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("--start-maximized");          
+            driver = new ChromeDriver(options);
+        }
+
+        public static IWebDriver getDriver()
+        {
+            return driver;
+        }
+        public static void takeScreenshot()
+        {
+            string screenshotPath = $"{AppDomain.CurrentDomain.BaseDirectory}screenshots";
+            string screenshotName = $"{screenshotPath}\\src-{Guid.NewGuid()}.png";
+
+            Directory.CreateDirectory(screenshotPath);
+            Screenshot screenshot = ((ITakesScreenshot)Driver.getDriver()).GetScreenshot();
+            screenshot.SaveAsFile(screenshotName, ScreenshotImageFormat.Png);
+        }
+        public static void closeDriver()
+        {
+            driver.Close();
+        }
+    }
+}
---
+++ Tests/BaseTests.cs
@@ -0,0 +1,25 @@
+﻿using Framework;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+
+namespace Tests.BaseTests
+{
+    public class BaseTests
+    {
+        [SetUp]
+        public static void setup()
+        {
+            Driver.getDriver();
+        }
+        [TearDown]
+        public static void close()
+        {
+            if(TestContext.CurrentContext.Result.Outcome != ResultState.Success)
+            {
+                Driver.takeScreenshot();
+            }
+            Driver.closeDriver();
+        }
+            
+    }
+}
